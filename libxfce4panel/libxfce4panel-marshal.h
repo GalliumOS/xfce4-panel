a06868ecf519876f5adb98906d2398c9e40db0af
Hugh Greenberg <hugegreenbug@gmail.com>
2015-08-28 10:06:33 -0600

    Initial commit

---
+++ libxfce4panel/libxfce4panel-marshal.h
@@ -0,0 +1,28 @@
+
+#ifndef ___libxfce4panel_marshal_MARSHAL_H__
+#define ___libxfce4panel_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* BOOLEAN:INT (libxfce4panel-marshal.list:1) */
+G_GNUC_INTERNAL void _libxfce4panel_marshal_BOOLEAN__INT (GClosure     *closure,
+                                                          GValue       *return_value,
+                                                          guint         n_param_values,
+                                                          const GValue *param_values,
+                                                          gpointer      invocation_hint,
+                                                          gpointer      marshal_data);
+
+/* BOOLEAN:STRING,BOXED (libxfce4panel-marshal.list:2) */
+G_GNUC_INTERNAL void _libxfce4panel_marshal_BOOLEAN__STRING_BOXED (GClosure     *closure,
+                                                                   GValue       *return_value,
+                                                                   guint         n_param_values,
+                                                                   const GValue *param_values,
+                                                                   gpointer      invocation_hint,
+                                                                   gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* ___libxfce4panel_marshal_MARSHAL_H__ */
+
